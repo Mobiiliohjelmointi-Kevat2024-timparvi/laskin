3c9f0f0cf638a8bae62812ee7523ba5757d4e6f3
Timo Parviainen <timo.parviaine@gmail.com>
2024-01-23 19:25:18 +0200

    simple calculator done

--- App.js
+++ App.js
@@ -1,10 +1,49 @@
+import {useState} from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput, SafeAreaView } from 'react-native';
 
 export default function App() {
+  const [numOne, setNumOne] = useState('');
+  const [numTwo, setNumTwo] = useState('');
+  const [result, setResult] = useState('');
+
+
+  const sumNumbers = () => {
+    const sum = parseInt(numOne) + parseInt(numTwo);
+    setResult(sum.toString());
+  }
+
+  const subtractNumbers = () => {
+    const difference = parseInt(numOne) - parseInt(numTwo);
+    setResult(difference.toString());
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Result: {result}</Text>
+      <TextInput
+      style={styles.input}
+      keyboardType='numeric'
+      value={numOne}
+      onChangeText={(text) => {
+        let newText = text.replace(/[^0-9]/g, '');
+        setNumOne(newText);
+      }}
+      />
+      <View style={styles.buttonContainer}>
+      <Button title= "+" onPress={sumNumbers} />
+      <Button title= "-" onPress={subtractNumbers} />
+      </View>
+      <TextInput
+      style={styles.input}
+      keyboardType='numeric'
+      value={numTwo}
+      onChangeText={(text) => {
+        let newText = text.replace(/[^0-9]/g, '');
+        setNumTwo(newText);
+      }}
+      />
+     
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +56,16 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  input: {
+    height: 40,
+    width: 80,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    width: '20%', 
+    justifyContent: 'space-around', // This spreads the buttons out evenly
+  }
 });
